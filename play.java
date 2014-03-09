13609a150f452049ab7df95a9bc2688508e88f01
walshyb <walshb1@hawkmail.newpaltz.edu>
2014-03-09 15:38:03 -0400

    Changed input so only column number is needed to be entered; players do not need to input their color value, color is chosen for each player

--- play.java
+++ play.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
-public class play 
+public class play
 {
-    public static String[][] board = new String[7][7];
-    public static Scanner kb = new Scanner(System.in);
-    public static String p1 = "", p2 ="";
-
-    public static void main(String[] args) 
+    private static String[][] board = new String[7][7];
+    private static Scanner kb = new Scanner(System.in);
+    private static String p1 = "", p2 ="";
+    private static int play = 1;
+    private static String value = "";
+    
+    public static void main(String[] args)
     {
-        makeBoard();
-        addPiece(4,5, "r");
+    	intro();
         showBoard();
+        turn();
+        turn();
+        turn();
+        turn();
+        turn();
     }
     
     //-----------------------------------------------------
@@ -31,22 +37,21 @@ public class play
     public static void makeBoard()
     {
     	for (int row = 0; row < board.length; row++)
-        {   
+        {
             for(int col = 0; col < board[row].length; col++)
             {
                 board[col][row] = "0";
             }
         }
-    	
     }
     
     //------------------------------------------------------
-
+    
     public static void showBoard()
-    {       	
+    {
     	System.out.print("\n      0  1  2  3  4  5  6  ");
         System.out.print("\n    ---------------------");
-
+        
         for (int row = 0; row < board.length; row++)
         {
             System.out.println("");
@@ -62,11 +67,11 @@ public class play
                 
                 if(board[col][row] == null)
                 {
-                  System.out.print("  ");
+                    System.out.print("  ");
                 }
                 else
                 {
-                  System.out.print(board[col][row] + "  ");
+                    System.out.print(board[col][row] + "  ");
                 }
             }
         }
@@ -76,10 +81,93 @@ public class play
     
     //------------------------------------------------------
     
-    public static void addPiece(int x, int y, String r)
+    public static void addPiece()
     {
-        board[x][y] = r;
-    }   
+    	System.out.println("Please enter the column number: ");
+    	int x = kb.nextInt();
+    	
+    	/*System.out.println("Plese enter the column number: ");
+         int y = kb.nextInt();*/
+		
+    	int y = 0;
+    	
+		if(!(play % 2 == 0))
+		{
+			value = "R";
+		}
+		else
+		{
+			value = "Y";
+		}
+		
+    	
+    	for(int row = 0; row < board.length; row++)
+    	{
+    		try
+    		{
+        		if(board[x][row] == "0")
+        		{
+        			board[x][row-1] = "0";
+        			board[x][row] = value;
+        		}
+    			
+        		
+    		}
+    		catch(ArrayIndexOutOfBoundsException a)
+    		{
+    			
+    		}
+    		
+    		play++;
+    		
+    	}
+    	
+    	/*if(play == 2)
+         {
+         for(int row = 0; row < board.length; row++)
+         {
+         board[x][row] = "0";
+         
+         if(!(board[x][row] == "0"))
+         {
+         if(!(play % 2 == 0))
+         {
+         value = "R";
+         }
+         else
+         {
+         value = "Y";
+         }
+         }
+         }
+         }*/
+		
+		
+		
+		/*while(true)
+         {
+         board[x][y+1] = "0";
+         
+         if(!(board[x][y] == "0"))
+         {
+         if(!(play % 2 == 0))
+         {
+         value = "R";
+         break;
+         }
+         else
+         {
+         value = "Y";
+         break;
+         }
+         
+         
+         }
+         }*/
+		
+        //board[x][y] = value;
+    	
+    }
     
     public static void play()
     {
@@ -87,7 +175,7 @@ public class play
     	while(true)
     	{
     		for (int row = 0; row < board.length; row++)
-            {   
+            {
                 for(int col = 0; col < board[row].length; col++)
                 {
                     if(!(board[col][row] == "0"))
@@ -103,18 +191,18 @@ public class play
     public static int checkWin() 	//in progress
     {
     	int counter1 = 0;
-    	 
+        
         /* horizontally */
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board.length; j++) {
                 if(board[i][j] == board[i][j + 1])
                     counter1++;
-     
+                
                 if(counter1 == 4)
                     return 1;
             }
         }
-     
+        
         /* vertically */
         
         int counter2 = 0;
@@ -122,13 +210,31 @@ public class play
             for(int b = 0; b < board.length; b++) {
                 if(board[b][a] == board[b + 1][a])
                     counter2++;
-     
+                
                 if(counter2 == 4)
                     return 1;
             }
         }
-     
+        
         return 0;
-   	 
+        
+    }
+    
+    public static void turn()
+    {
+    	if(!(play % 2 == 0))
+    	{
+    		System.out.println("\n" + p1 + "'s turn");
+    		value = "R";
+    		addPiece();
+    	}
+    	else
+    	{
+    		System.out.println("\n" + p2 + "'s turn");
+    		value = "Y";
+    		addPiece();
+    	}
+    	
+    	showBoard();
     }
 }
